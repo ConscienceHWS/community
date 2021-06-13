b35dc66606fe826af987f88162872678a8257cf1
ConscienceHWS <915983300@qq.com>
2021-06-13 10:50:31 +0800

    init repo

---
+++ src/main/java/life/jack/community/controller/indexController.java
@@ -0,0 +1,11 @@
+package life.jack.community.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class indexController {
+    @GetMapping("/")
+    public String index () {
+        return "index";
+    }
+}
+
